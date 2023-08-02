dfe8752fbac7c58c17f6c80e984778436085c48a
Palina Karabovich <polina.korobovich@yandex.by>
2023-08-02 13:55:27 +0200

    directories created, header component crated

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css'
+import Header from '../Header/Header';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
