de0de99e532f25b4deec02adf5a0779accabdbdb
Palina Karabovich <polina.korobovich@yandex.by>
2023-08-02 14:36:26 +0200

    slider component created

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.module.css'
 import Header from '../Header/Header';
+import Slider from '../Slider/Slider';
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <Slider />
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import { cartIcon } from '../../utils/icons';
 
 const Header = () => {
-  return (  
-    <header>
-      header
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <a className={`${styles.link} ${styles.link_active}`}>SALE</a>
+        <a className={styles.link}>Furniture</a>
+        <a className={styles.link}>Accessories</a>
+        <a className={styles.link}>About</a>
+        <a className={styles.link}>Contacts</a>
+      </nav>
+      <a className={`${styles.link} ${styles.cart}`}>
+        <div className={styles.icon}>
+          {cartIcon}
+        </div>
+        <div className={styles.quantity}>
+          2
+        </div>
+      </a>
     </header>
-  )
-}
- 
+  );
+};
+
 export default Header;
