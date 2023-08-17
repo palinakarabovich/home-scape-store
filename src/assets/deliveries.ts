492cf37e10b17fefcc96f955fb0a790afa7cf38b
Palina Karabovich <polina.korobovich@yandex.by>
2023-08-17 15:40:27 +0200

    delivery and payment options created

---
+++ src/assets/deliveries.ts
@@ -0,0 +1,17 @@
+export const deliveries = [
+  {
+    name: 'Standart delivery',
+    duration: '10-15 workdays',
+    price: 'FREE'
+  },
+  {
+    name: 'Express delivery',
+    duration: '5-7 workdays',
+    price: '20'
+  },
+  {
+    name: 'Next-day',
+    duration: '1-2 workdays(depending on time you made your order)',
+    price: '50'
+  },
+]
--- src/components/CheckoutSelection/CheckoutSelection.tsx
+++ src/components/CheckoutSelection/CheckoutSelection.tsx
@@ -1,47 +1,47 @@
+import React from 'react';
 import styles from './CheckoutSelection.module.css'
+import { payments } from '../../assets/paymentMethods';
+import { deliveries } from '../../assets/deliveries';
 
 const CheckoutSelection = () => {
+
+  const [payment, setPayment] = React.useState();
+  const [delivery, setDelivery] = React.useState();
+
+  const handleDeliveryClick = () => {
+
+  }
+
   return (
     <section className={styles.selection}>
       <h2 className={styles.title}>
         Select your payment method and delivery:
       </h2>
-      <div className={styles.group}>
-      <div className={styles.container}>
-          <input type='radio' name='payment' id='ideal' />
-          <label htmlFor='ideal' className={styles.label}>iDEAL</label>
+      <div className={styles.options}>
+        <div className={styles.group}>
+          {
+            payments.map((p) => <div className={styles.container}>
+              <input type='checkbox' id={p.name} className={styles.checkbox} />
+              <label htmlFor={p.name} className={styles.label}>{p.name}</label>
+              {
+                p.images.map((i) => <img src={i} className={styles.image} alt={p.name} />)
+              }
+            </div>)
+          }
         </div>
-        <div className={styles.container}>
-          <input type='radio' name='payment' id='card' />
-          <label htmlFor='card' className={styles.label}>Credit card</label>
-        </div>
-        <div className={styles.container}>
-          <input type='radio' name='payment' id='paypal' />
-          <label htmlFor='paypal' className={styles.label}>PayPal</label>
-        </div>
-      </div>
-      <div className={styles.group}>
-      <div className={styles.container}>
-          <input type='radio' name='delivery' id='standsrt' />
-          <label htmlFor='standart' className={styles.label}>Standart delivery</label>
-          <p className={styles.text}>10-15 workdays</p>
-          <p className={styles.price}>FREE</p>
-        </div>
-        <div className={styles.container}>
-          <input type='radio' name='delivery' id='express' />
-          <label htmlFor='express' className={styles.label}>Express delivery</label>
-          <p className={styles.text}>5-7 workdays</p>
-          <p className={styles.price}>+20 €</p>
-        </div>
-        <div className={styles.container}>
-          <input type='radio' name='delivery' id='next-day' />
-          <label htmlFor='next-day' className={styles.label}>Next-day</label>
-          <p className={styles.text}>1-2 workdays</p>
-          <p className={styles.price}>+50 €</p>
+        <div className={styles.group}>
+          {
+            deliveries.map((d) => <div className={styles.container}>
+              <input type='checkbox' id={d.name} className={styles.checkbox} />
+              <label htmlFor='next-day' className={styles.label}>{d.name}</label>
+              <p className={styles.text}>{d.duration}</p>
+              <p className={styles.price}>{d.price === "FREE" ? d.price : `+${d.price} €`}</p>
+            </div>)
+          }
         </div>
       </div>
     </section>
   );
 }
- 
+
 export default CheckoutSelection;
